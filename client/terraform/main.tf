5329fa4516b7b91d8d67b2e02813cb922fcc24fe
Eric Ventor <ericventor97@gmail.com>
2023-07-21 16:13:20 -0700

    client terraform resources

--- client/terraform/main.tf
+++ client/terraform/main.tf
@@ -1,7 +1,9 @@
+# Bucket used to host your static website files
 resource "aws_s3_bucket" "client-bucket" {
   bucket = var.domain_name
 }
 
+# Bucket policy to enable public read access
 resource "aws_s3_bucket_policy" "client-bucket-policy" {
   bucket = aws_s3_bucket.client-bucket.id
   policy = jsonencode(
@@ -18,4 +20,17 @@ resource "aws_s3_bucket_policy" "client-bucket-policy" {
       ]
     }
   )
+}
+
+# Website config that specifies the index and error documents
+# If using a SPA framework, you'll want to set the error document to index.html
+resource "aws_s3_bucket_website_configuration" "beta-test-songs-frontend-hosting" {
+  bucket = aws_s3_bucket.beta-test-songs-frontend-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
 }
