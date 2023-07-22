10edec21cdec99d3e123f0834220dd6dfa54e905
Eric Ventor <ericventor97@gmail.com>
2023-07-21 21:01:12 -0700

    added necessary s3 terraform reosurces

--- client/terraform/main.tf
+++ client/terraform/main.tf
@@ -3,8 +3,36 @@ resource "aws_s3_bucket" "client-bucket" {
   bucket = var.domain_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "client-bucket-ownership-controls" {
+  bucket = aws_s3_bucket.client-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "client-bucket-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.client-bucket-ownership-controls,
+    aws_s3_bucket_public_access_block.client-bucket-access,
+  ]
+
+  bucket = aws_s3_bucket.client-bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "client-bucket-access" {
+  bucket = aws_s3_bucket.client-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 # Bucket policy to enable public read access
 resource "aws_s3_bucket_policy" "client-bucket-policy" {
+  depends_on = [aws_s3_bucket_public_access_block.client-bucket-access]
+
   bucket = aws_s3_bucket.client-bucket.id
   policy = jsonencode(
     {
